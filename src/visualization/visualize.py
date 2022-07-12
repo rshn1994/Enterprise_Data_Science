c65e63236d96ef980c5d5a04db791bf7ce475488
rshn1994 <shenoy.roshan@outlook.com>
2022-07-12 23:24:34 +0200

    modified the notebook Covid_Dashboard.ipynb to plot relative cases

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -3,8 +3,8 @@ import numpy as np
 
 import dash
 dash.__version__
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output,State
 
 import plotly.graph_objects as go
